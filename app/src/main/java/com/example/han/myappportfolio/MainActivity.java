889efd80761411b358ae18fb2323554ea871086a
qinwei <591356206@qq.com>
2016-09-22 00:40:39 +0800

    completed

--- app/src/main/java/com/example/han/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/han/myappportfolio/MainActivity.java
@@ -2,12 +2,50 @@ package com.example.han.myappportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
+    Button btn6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+
+        Log.d(TAG, "onCreate: " + btn1.getText());
+
+        View.OnClickListener onClick =new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button btn = (Button) v;
+                Toast.makeText(getApplication(),"This button will launch my " + btn.getText() + " app",Toast.LENGTH_LONG).show();
+            }
+        };
+
+        btn1.setOnClickListener(onClick);
+        btn2.setOnClickListener(onClick);
+        btn3.setOnClickListener(onClick);
+        btn4.setOnClickListener(onClick);
+        btn5.setOnClickListener(onClick);
+        btn6.setOnClickListener(onClick);
+
     }
 }
